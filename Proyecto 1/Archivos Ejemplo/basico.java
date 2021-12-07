80de8f9a42e2f5efdadd660034539eb4f2d3dd2c
Haroldo Arias <ihpam25@gmail.com>
2021-12-07 11:13:22 -0600

    Update basico.java

--- Proyecto 1/Archivos Ejemplo/basico.java
+++ Proyecto 1/Archivos Ejemplo/basico.java
@@ -9,7 +9,7 @@ void main(){
     println("El valor de var1 local es $a");  //25
 
     println("Probando expresiones Arítmeticas");
-    println(-25^(69-33*2)+22-32*2-33*(-48+48));   // -62
+    println(-25*(69-33*2)+22-32*2-33*(-48+48));   // -62
     println(-93.555+92.12-81.33+19+26-68+-7/(79+11)/86);    // -105.765
     println(8+67+74-1.0*((-86+22)*2)-5*6);  // 247.0
     println((51 % 49) * (9.9+90.1));    // 200.0
